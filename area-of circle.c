50c68c8f39ac31dcd438a90b1256f5639106155a
Divyanshibhatt <divyanshib2@gmail.com>
2025-08-22 00:47:01 +0530

    day 2 ques 2

--- area-of circle.c
+++ area-of circle.c
@@ -4,10 +4,10 @@ Write a program to calculate the area and circumference of a circle given its ra
 int main()
 {
     float area=0,circumference=0,radius=0,pi=3.14; //area=3.14*(radius**2),circumference=2*3.14*radius
-    scanf("%d",&radius);  //input
+    scanf("%f",&radius);  //input
     area=pi*(radius*radius);
-    circumference=2*pi*radius;
-    printf("area of circle is=%d\n",area);
-        printf("circumference of circle is =%d\n",circumference);
+    circumference= 2*pi*radius;
+    printf("area of circle is=%f\n",area);
+        printf("circumference of circle is =%f\n",circumference);
         return 0;
 }
