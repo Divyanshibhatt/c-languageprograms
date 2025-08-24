9ec600bd784bf32704d5e1ecdec1becc15426451
Divyanshibhatt <divyanshib2@gmail.com>
2025-08-25 00:00:46 +0530

    day 3 ques 2

--- add.c
+++ add.c
@@ -6,7 +6,7 @@ Write a program to input two numbers and display their sum.*/
 int main()
 {
     int x=0,y=0;
-    scanf("%d%d",&x,&y);
+    scanf("%d%d",&x,&y); //user input 
     printf("the sum of two numbers is = %d\n",x+y);
     return 0;
 }
