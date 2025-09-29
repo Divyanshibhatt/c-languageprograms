3fd25e8b3a9090ae62b5ec0dc94b53e996bd4db9
Divyanshibhatt <divyanshib2@gmail.com>
2025-09-29 10:20:09 +0530

    day 39 ques 77

--- day39-2Darray/distinct.c
+++ day39-2Darray/distinct.c
@@ -1,7 +1,8 @@
 /*Q77 (2D Arrays)
 Check if the elements on the diagonal of a matrix are distinct.*/
 #include <stdio.h>
-int main() {
+int main() 
+{
     int matrix[10][10], n, i, j;
     int distinct[10], k = 0, isDistinct = 1;
 
@@ -9,33 +10,41 @@ int main() {
     scanf("%d", &n);
 
     printf("Enter the elements of the matrix:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++) 
+        {
             scanf("%d", &matrix[i][j]);
         }
     }
 
     // Extract diagonal elements
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         distinct[k++] = matrix[i][i];
     }
 
     // Check for distinctness
-    for (i = 0; i < k; i++) {
-        for (j = i + 1; j < k; j++) {
+    for (i = 0; i < k; i++) 
+    {
+        for (j = i + 1; j < k; j++) 
+        {
             if (distinct[i] == distinct[j]) {
                 isDistinct = 0;
                 break;
             }
         }
-        if (!isDistinct) {
+        if (!isDistinct)
+         {
             break;
         }
     }
 
-    if (isDistinct) {
+    if (isDistinct) 
+    {
         printf("The diagonal elements are distinct.\n");
-    } else {
+    } else 
+    {
         printf("The diagonal elements are not distinct.\n");
     }
 
